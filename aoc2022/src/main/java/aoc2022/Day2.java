d202d2af3c313ed38c20e0c6781e74ba96276223
Wojciech Kromolicki <wojciech.kromolicki@gmail.com>
2022-12-02 13:21:30 +0100

    constants1 (#2)

--- aoc2022/src/main/java/aoc2022/Day2.java
+++ aoc2022/src/main/java/aoc2022/Day2.java
@@ -6,6 +6,7 @@ import common.Pair;
 import java.util.Map;
 
 import static aoc2022.Common.println;
+import static aoc2022.Day2.Constants.*;
 
 public class Day2 {
     public static void main(String[] args) {
@@ -23,39 +24,50 @@ public class Day2 {
 
     }
     //A,X - Rock1, B,Y - Paper2, C,Z -> Scissors3
+
     static int part1(String line) {
         return switch(line) {
-            case "A X" -> 3 + 1;
-            case "A Y" -> 6 + 2;
-            case "A Z" -> 0 + 3;
+            case "A X" -> DRAW + ROCK;
+            case "A Y" -> WIN + PAPER;
+            case "A Z" -> LOSS + SCISSORS;
 
-            case "B X" -> 0 + 1;
-            case "B Y" -> 3 + 2;
-            case "B Z" -> 6 + 3;
+            case "B X" -> LOSS + ROCK;
+            case "B Y" -> DRAW + PAPER;
+            case "B Z" -> WIN + SCISSORS;
 
-            case "C X" -> 6 + 1;
-            case "C Y" -> 0 + 2;
-            case "C Z" -> 3 + 3;
+            case "C X" -> WIN + ROCK;
+            case "C Y" -> LOSS + PAPER;
+            case "C Z" -> DRAW + SCISSORS;
 
-            default -> 0;
+            default -> NOTHING;
         };
     }
     //A - Rock1, B - Paper2, C -> Scissors3, X lose, Y draw, Z win
     static int part2(String line) {
         return switch(line) {
-            case "A X" -> 0 + 3;
-            case "A Y" -> 3 + 1;
-            case "A Z" -> 6 + 2;
+            case "A X" -> LOSS + SCISSORS;
+            case "A Y" -> DRAW + ROCK;
+            case "A Z" -> WIN + PAPER;
 
-            case "B X" -> 0 + 1;
-            case "B Y" -> 3 + 2;
-            case "B Z" -> 6 + 3;
+            case "B X" -> LOSS + ROCK;
+            case "B Y" -> DRAW + PAPER;
+            case "B Z" -> WIN + SCISSORS;
 
-            case "C X" -> 0 + 2;
-            case "C Y" -> 3 + 3;
-            case "C Z" -> 6 + 1;
+            case "C X" -> LOSS + PAPER;
+            case "C Y" -> DRAW + SCISSORS;
+            case "C Z" -> WIN + ROCK;
 
             default -> 0;
         };
+    }
+
+    public static class Constants {
+        static int WIN = 6;
+        static int DRAW = 3;
+        static int LOSS = 0;
+        static int ROCK = 1;
+        static int PAPER = 2;
+        static int SCISSORS = 3;
+        static int NOTHING = 0;
     }
 }
