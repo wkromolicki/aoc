b60a4f4936e91d7777dd733652d6a4a9764e403f
Wojciech K <wojciech.kromolicki@gmail.com>
2022-12-05 12:12:33 +0100

    day3 (#3)

---
+++ aoc2022/src/main/java/aoc2022/Day3.java
@@ -0,0 +1,58 @@
+package aoc2022;
+
+import common.CollectionOps;
+import common.Common;
+import common.Common.Strings;
+import common.Pair;
+
+import java.util.List;
+import java.util.Set;
+import java.util.function.ToIntFunction;
+import java.util.stream.Collectors;
+
+import static common.CollectionOps.groupBy;
+import static common.CollectionOps.toSet;
+import static common.Common.Strings.splitInHalf;
+import static common.Common.println;
+
+public class Day3 {
+
+    public static void main(String[] args) {
+
+        var sum = Common.lines("aoc2022/day3.txt").stream().mapToInt(l -> sumPriorities(commonTypes(splitInHalf(l)), Priorities::priority)).sum();
+
+        println("Part1: ", sum);
+
+        var groupedBy3 = groupBy(Common.lines("aoc2022/day3.txt").stream().map(Strings::characters).collect(Collectors.toList()), 3);
+
+        var sumBadges = groupedBy3.stream().mapToInt(Day3::badges).sum();
+        println("Part2: " + sumBadges);
+
+    }
+
+    static Set<String> commonTypes(Pair<String, String> rucksack) {
+        var left = Strings.characters(rucksack.a());
+        var right = Strings.characters(rucksack.b());
+        return CollectionOps.intersect(toSet(left), toSet(right));
+    }
+
+    static int badges(List<List<String>> threeSets) {
+        var badges = CollectionOps.intersect(threeSets.stream().map(CollectionOps::toSet).collect(Collectors.toList()));
+
+        return sumPriorities(badges, Priorities::priority);
+    }
+
+    static int sumPriorities(Set<String> input, ToIntFunction<String> priority) {
+        return input.stream().mapToInt(priority).sum();
+    }
+public static class Priorities {
+    static String p = "abcdefghijklmnopqrstuvwxyz";
+    static String p2 = p + p.toUpperCase();
+
+    static int priority(String s) {
+        return p2.indexOf(s) + 1;
+    }
+}
+}
+
+
---
+++ aoc2022/src/main/java/common/CollectionOps.java
@@ -0,0 +1,88 @@
+package common;
+
+import java.util.*;
+import java.util.function.Predicate;
+
+public class CollectionOps {
+
+    public static <T> Set<T> intersect(Set<T> left, Set<T> right) {
+        return intersect(List.of(left,right));
+    }
+
+    public static <T> Set<T> toSet(Collection<T> c) {
+        return new HashSet<>(c);
+    }
+
+    public static <T> Set<T> intersect(List<Set<T>> sets) {
+
+
+        if (sets.isEmpty()) {
+            return Set.of();
+        } else {
+            var intersection = new HashSet<>(sets.stream().findFirst().get());
+
+            sets.forEach(intersection::retainAll);
+            return intersection;
+        }
+
+    }
+
+    public static <T> List<T> merge(List<T> c1, List<T> c2) {
+        var r = new ArrayList<T>();
+        r.addAll(c1);
+        r.addAll(c2);
+        return r;
+    }
+
+    public static <T> List<T> add(List<T> list, T item){
+        return merge(list, List.of(item));
+    }
+
+    public static <T> List<T> copy(List<T> c1) {
+        return merge(c1, List.of());
+    }
+    public static <I> List<List<I>> groupWhen(
+            List<I> input,
+            Predicate<I> newGroupCondition
+    ){
+        var result = input.stream().collect(() -> new Pair<>(new ArrayList<List<I>>(), new ArrayList<I>()), (acc, val) -> {
+            switch(val) {
+                case I i when newGroupCondition.test(i) -> {
+                    acc.b().add(i);
+                    acc.a().add(copy(acc.b()));
+                    acc.b().clear();
+                }
+                case I l -> {
+                    acc.b().add(l);
+                }
+            }
+        }, (p1, p2) -> new Pair<>(merge(p1.a(), p2.a()), merge(p1.b(), p2.b())));
+        return add(result.a(), result.b());
+    }
+
+    public static <I> List<List<I>> groupBy(List<I> input, int size) {
+        var counter = new ResettingCounter(size);
+        return groupWhen(input, c -> counter.wasReset());
+    }
+
+
+    public static class ResettingCounter{
+        private final int count;
+
+        private int v = 0;
+        public ResettingCounter(int count) {
+            this.count = count;
+        }
+
+        public boolean wasReset() {
+            v = v +1;
+            if(v < count) {
+                return false;
+            } else {
+                v = 0;
+                return true;
+            }
+
+        }
+    }
+}
