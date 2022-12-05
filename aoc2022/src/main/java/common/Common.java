ec4d86971a041505c6631539489e4b99ebbde0b9
Wojciech K <wojciech.kromolicki@gmail.com>
2022-12-05 12:46:07 +0100

    day4 (#4)

--- aoc2022/src/main/java/common/Common.java
+++ aoc2022/src/main/java/common/Common.java
@@ -30,9 +30,13 @@ public class Common {
         };
     }
 
-    public static class Strings {
+    public static class Strings2 {
         public static Pair<String, String> splitInHalf(String s){
             return Pair.of(s.substring(0, s.length() / 2), s.substring(s.length() / 2 ));
+        }
+
+        public static Pair<String, String> splitOn(String s, String c){
+            return Pair.of(s.substring(0, s.indexOf(c)), s.substring(s.indexOf(c) + 1));
         }
 
         public static List<String> characters(String s) {
