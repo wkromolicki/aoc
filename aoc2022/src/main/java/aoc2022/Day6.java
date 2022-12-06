abc97fc472767923a052618262f2803ef79cc748
kroma <wojciech.kromolicki@gmail.com>
2022-12-06 14:00:40 +0100

    day6 oneliner

--- aoc2022/src/main/java/aoc2022/Day6.java
+++ aoc2022/src/main/java/aoc2022/Day6.java
@@ -3,7 +3,11 @@ package aoc2022;
 import common.Console2;
 import common.Strings2;
 
+import java.util.*;
+
 import static common.Console2.println;
+import static java.lang.Math.max;
+import static java.util.Arrays.asList;
 
 public class Day6 {
     public static void main(String[] args) {
@@ -12,12 +16,21 @@ public class Day6 {
         println("part1: ",findNDifferentSymbols(line, 4));
 
         println("part2: ", findNDifferentSymbols(line, 14));
+
+        println("part1x:", solve(line, 4));
     }
 
     static int findNDifferentSymbols(String line, int n) {
         var distinctBuffer = new DistinctBuffer(n);
         var allChars = Strings2.characters(line).stream().takeWhile(distinctBuffer::isNotFull).count();
+
         return (int)allChars + 1;
+    }
+
+    static int solve(String line, int n) {
+        for(int p=0; p < line.length(); p++)
+            if (Set.copyOf(asList(line.substring(max(0, p - n), p).split(""))).size() == n) return p;
+        return -1;
     }
 
     static class DistinctBuffer {
